022820ef58fe866849b561d717acee56f4ae2942
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2026-07-09 10:10:16 +0530

    Time: 3 ms (28.02%) | Memory: 28.3 MB (10.10%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // better approch
-        if(nums.size() == 1) return nums[0];
-        sort(nums.begin(),nums.end());
-        int cnt = 1;
-        int ans = nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] == nums[i-1]){
-                cnt++;
-            }else{
-                cnt = 1;
+        // optimal approach moore voting algo
+        int ans  = 0;
+        int cnt = 0;
+        for(int i=0;i<nums.size();i++){
+            if(cnt == 0){
                 ans = nums[i];
             }
-            if(cnt > nums.size()/2) return ans;
+            if(ans == nums[i]){
+                cnt++;
+            }else{
+                cnt--;
+            }
         }
 
-        
+        int freq = 0;
+        for(int x: nums){
+            if(x == ans){
+                freq++;
+            }
+        }
+
+        if(freq > nums.size() / 2) return ans;
+
         return -1;
     }
 };
