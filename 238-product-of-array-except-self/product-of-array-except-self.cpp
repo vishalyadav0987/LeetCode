45df8c0d3120e17a856be058277f89facd78d6a9
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2026-07-16 21:27:48 +0530

    Time: 4 ms (21.19%) | Memory: 42.2 MB (17.53%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int product = 1;
-        int cntZero = 0;
-        int productExceptZero = 1;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 0) {
-                cntZero++;
-            } else {
-                productExceptZero *= nums[i];
-            }
-            if (cntZero == 2) {
-                productExceptZero *= 0;
-            }
-            product *= nums[i];
+        int n = nums.size();
+        vector<int> prefix(n,1);
+        vector<int> suffix(n,1);
+        vector<int> ans(n,1);
+
+        // prefix
+        // [1,1,2,6]
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1] * nums[i-1];
         }
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (cntZero >= 2) {
-                nums[i] = nums[i] * 0;
-            } else if (cntZero == 1) {
-                if (nums[i] != 0) {
-                    nums[i] = 0 * nums[i];
-                } else {
-                    nums[i] = productExceptZero;
-                }
-            } else {
-                nums[i] = product / nums[i];
-            }
+        // suffix
+        // [24,12,4,1]
+        for(int i=n-2;i>=0;i--){
+            suffix[i] = suffix[i+1] * nums[i+1];
         }
 
-        return nums;
+        for(int i=0;i<n;i++ ){
+            ans[i] = prefix[i]*suffix[i];
+        }
+
+        return ans;
     }
 };
