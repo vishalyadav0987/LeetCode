2026884f6bc22b10ea4b6a849e09ac507a408c86
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2026-07-31 23:51:45 +0530

    Time: 383 ms (5.02%) | Memory: 53.2 MB (28.80%) - LeetSync

---
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int matrixSum(vector<vector<int>>& nums) {
+        int r = nums.size();
+        int c = nums[0].size();
+        int totalScore = 0;
+
+        for (int i = 0; i < c; i++) {
+            int maxEle = -1;
+            for (int j = 0; j < r; j++) {
+                int maxOFRow = *max_element(nums[j].begin(), nums[j].end()); 
+                maxEle = max(maxEle, maxOFRow);
+                int idx = max_element(nums[j].begin(), nums[j].end()) -
+                          nums[j].begin();
+                nums[j].erase(nums[j].begin() + idx);
+            }
+
+            totalScore += maxEle;
+        }
+
+        return totalScore;
+    }
+};
