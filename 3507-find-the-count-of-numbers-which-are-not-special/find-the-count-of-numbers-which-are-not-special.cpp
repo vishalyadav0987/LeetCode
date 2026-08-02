5121aa81e863be9776623459c4c7262f2f8930ba
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2026-08-02 09:45:16 +0530

    Time: 143 ms (18.28%) | Memory: 12.2 MB (27.24%) - LeetSync

---
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int nonSpecialCount(int l, int r) {
+        int limit = sqrt(1000000000);
+        vector<bool> isPrime(limit + 1, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= limit; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= limit; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int special = 0;
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime[i]) {
+                long long square = 1LL * i * i;
+
+                if (square >= l && square <= r) {
+                    special++;
+                }
+            }
+        }
+
+        return (r - l + 1) - special;
+    }
+};
