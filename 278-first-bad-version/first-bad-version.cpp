f63cc236b29c30db715e0ba83981f0bd24804a90
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2026-08-29 22:04:03 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (5.20%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,24 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        // Afain doing 29-August-2026
+       int low=1;
+        int high=n;
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid)){
+                high=mid;
+                // When isBadVersion(mid) returns true, it means that 
+                // version mid is a bad version. However, since we want to find the 
+                // first bad version, we cannot rule out mid itself as being the first bad one.
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
