031447e4fa46a8affbb8768274809a9f12f138a5
vishal <118720096+vishalyadav0987@users.noreply.github.com>
2026-09-19 14:24:31 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (55.15%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -4,9 +4,10 @@ public:
         int n = mat[0].size();
         if(n==1) return mat[0][0];
         int sum = 0;
-        // primary diagonal
         for(int i=0;i<n;i++){
+            // primary diagonal
             sum+=mat[i][i];
+            // secondary diagonal
             sum+=mat[i][n-i-1];
         }
 
